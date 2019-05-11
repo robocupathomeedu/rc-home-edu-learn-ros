512cf4531736842ca099682f9aea9bb4eb459800
Luca Iocchi <iocchi@dis.uniroma1.it>
2019-05-11 13:14:09 +0200

    objrec returns also confidence [0,100]

--- rchomeedu_vision/scripts/mobilenet_objrec.py
+++ rchomeedu_vision/scripts/mobilenet_objrec.py
@@ -115,7 +115,7 @@ class MNetObjRec:
                 cbest = c[0]
         sr += '\n  >>> %s %.2f <<<\n' %(cbest,pbest*100)
         print(sr)
-        return cbest
+        return (cbest,pbest*100)
 
 
 
