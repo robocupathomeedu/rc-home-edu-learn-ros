d449d985a4f285b398ec56f15ad8295bea42273e
Luca Iocchi <iocchi@diag.uniroma1.it>
2020-02-16 22:38:53 +0100

    docker run revised

--- docker/run.bash
+++ docker/run.bash
@@ -4,55 +4,55 @@
 
 IMAGENAME=ros-kinetic-rchomeedu
 VERSION=0.4
-
 if [ ! "$1" == "" ]; then
   VERSION=$1
 fi
 
-echo "Running image $IMAGENAME:$VERSION ..."
-
+# change setings here if needed
 ROBOT_DEVICE=/dev/ttyACM0
-
 CAMERA_DEVICE=/dev/video0
+PLAYGROUND_FOLDER=$HOME/playground
+
+
+echo "Running image $IMAGENAME:$VERSION ..."
 
 if [ -f $ROBOT_DEVICE ]; then
   echo "Robot device $ROBOT_DEVICE enabled"
-else
-  ROBOT_DEVICE=/dev/null
+  ROBOT_DEVICE_STR="--device=$ROBOT_DEVICE"
+fi
+
+if [ -f $CAMERA_DEVICE ]; then
+  echo "Camera device $CAMERA_DEVICE enabled"
+  CAMERA_DEVICE_STR="--device=$CAMERA_DEVICE"
 fi
 
 if [ -d /usr/lib/nvidia-384 ]; then
-  NVIDIASTR="-v /usr/lib/nvidia-384:/usr/lib/nvidia-384 \
+  NVIDIA_STR="-v /usr/lib/nvidia-384:/usr/lib/nvidia-384 \
            -v /usr/lib32/nvidia-384:/usr/lib32/nvidia-384 \
            --device /dev/dri"
   echo "Nvidia support enabled"
-else
-  NVIDIASTR=""
 fi
 
 if [ -d /run/user/$(id -u)/pulse ]; then
-  AUDIOSTR="--device=/dev/snd \
+  AUDIO_STR="--device=/dev/snd \
            -v /run/user/$(id -u)/pulse:/run/user/1000/pulse \
            -v $HOME/.config/pulse/cookie:/home/robot/.config/pulse/cookie"
   echo "Audio support enabled"
-else
-  AUDIOSTR=""
 fi
 
 chmod go+rw ~/.config/pulse/cookie # this file needed by docker user
 
-PLAYGROUND_FOLDER=$HOME/playground
 
 docker run -it \
     -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
     -v $HOME/.Xauthority:/home/robot/.Xauthority:rw \
-    $NVIDIASTR \
+    $NVIDIA_STR \
     -e DISPLAY=$DISPLAY \
     --privileged \
     --net=host \
-    --device=$ROBOT_DEVICE \
-    --device=$CAMERA_DEVICE \
-    $AUDIOSTR \
+    $ROBOT_DEVICE_STR \
+    $CAMERA_DEVICE_STR \
+    $AUDIO_STR \
     -v $PLAYGROUND_FOLDER:/home/robot/playground \
     $IMAGENAME:$VERSION
 
